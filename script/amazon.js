f6c5490b948793220d5f23ff3a7992ba75f59012
makertouch <153662956+makertouch@users.noreply.github.com>
2024-03-14 08:57:15 +0200

    Update amazon.js

--- script/amazon.js
+++ script/amazon.js
@@ -1,6 +1,7 @@
 import {cart} from '../data/cart.js';
 import {products} from '../data/products.js';
-
+console.log(products);
+console.log(cart);
 let productsHTML = ``;
 
 products.forEach((product) => {
