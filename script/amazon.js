e017bde002df02eb77c9b6ff1cf236e92aaf390a
makertouch <153662956+makertouch@users.noreply.github.com>
2024-03-17 12:52:09 +0200

    Update amazon.js

--- script/amazon.js
+++ script/amazon.js
@@ -60,25 +60,6 @@ productsHTML += `
 
 document.querySelector(`.js-products-grid`).innerHTML = productsHTML;
 
-function addToCart(productId) {
-
-	let matchingItem;
-
-cart.forEach((cartItem) => {
-if (productId === cartItem.productId) {
-matchingItem = cartItem;  /* make a shortcut of the item (refrences) */
-}
-}); 
-
-if (matchingItem) {
-matchingItem.quantity += 1;
-} else {
-cart.push({
-productId: productId,
-quantity: 1
-});
-}
-}
 
 function updateCartQunatity() {
 	
