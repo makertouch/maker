5451b8665ac8cd6dbbccc34a314a363c8c0d49a0
makertouch <153662956+makertouch@users.noreply.github.com>
2024-03-20 12:52:00 +0200

    Update checkout.js

--- script/checkout.js
+++ script/checkout.js
@@ -3,6 +3,7 @@ import { products } from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 
 let checkOutHTML = '';
+let radioSelector = 0;
 
 // Transfer info to cart from products after indentify the same id.
 cart.forEach((cartItem) => {
@@ -53,7 +54,7 @@ $${formatCurrency(matchingProduct.priceCents)}
                 <div class="delivery-option">
                   <input type="radio" checked
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${radioSelector += 1}">
                   <div>
                     <div class="delivery-option-date">
                       Tuesday, June 21
@@ -66,7 +67,7 @@ $${formatCurrency(matchingProduct.priceCents)}
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${radioSelector += 1}">
                   <div>
                     <div class="delivery-option-date">
                       Wednesday, June 15
@@ -79,7 +80,7 @@ $${formatCurrency(matchingProduct.priceCents)}
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${radioSelector += 1}">
                   <div>
                     <div class="delivery-option-date">
                       Monday, June 13
