8eb56fda07ab0376bd44d673b1fdaf1e5c541ad0
makertouch <153662956+makertouch@users.noreply.github.com>
2024-03-23 10:54:24 +0200

    Update checkout.js

--- script/checkout.js
+++ script/checkout.js
@@ -101,6 +101,8 @@ document.querySelectorAll(`.js-delete-link`)
   link.addEventListener(`click`, () => {
     const productId = link.dataset.productId;
     removeFromCart(productId);
-    classList.remove("js-cart-item-container-${productId}");
+    const container = document.querySelector(`.js-cart-item-container-${productId}`);
+    container.remove();
+  
   });
 });
