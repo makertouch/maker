ce9100716221c0b78d54319523eb7c87963308eb
makertouch <153662956+makertouch@users.noreply.github.com>
2024-03-24 21:43:52 +0200

    Update cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,17 @@
-export let cart = JSON.parse(localStorage.getItem(`cart`)) || [{
+export let cart = JSON.parse(localStorage.getItem(`cart`)); 
+if (!cart) {
+[{
 productId: `e43638ce-6aa0-4b85-b27f-e1d07eb678c6`,
 quantity: 2
 }, {
 productId: `15b6fc6f-327a-4ec4-896f-486349e85a3d`,
 quantity: 3	
 }
-];
+];	
+}
 
-function updateCart() {
+
+function saveToStorage() {
 	localStorage.setItem(`cart`, JSON.stringify(cart));
 }
 
@@ -29,7 +33,7 @@ productId: productId,
 quantity: 1
 });
 }
-	updateCart();
+	saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -42,5 +46,5 @@ cart.forEach((cartItem) => {
 	}
 }); 
 	cart = newCart;
-	updateCart();
+	saveToStorage();
 }
