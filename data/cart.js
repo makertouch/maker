aae99cb58c7eb9a7f9f3b106fd97ea66c695b85e
makertouch <153662956+makertouch@users.noreply.github.com>
2024-03-24 21:30:53 +0200

    save Cart cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem(`cart`)) || [{
 productId: `e43638ce-6aa0-4b85-b27f-e1d07eb678c6`,
 quantity: 2
 }, {
@@ -42,4 +42,5 @@ cart.forEach((cartItem) => {
 	}
 }); 
 	cart = newCart;
+	updateCart();
 }
