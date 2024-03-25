0d03f61a12365072f3742b4b1a34070bf8747d3d
makertouch <153662956+makertouch@users.noreply.github.com>
2024-03-25 16:26:08 +0200

    Update checkout.js

--- script/checkout.js
+++ script/checkout.js
@@ -103,7 +103,7 @@ document.querySelectorAll(`.js-delete-link`)
     removeFromCart(productId);
     const container = document.querySelector(`.js-cart-item-container-${productId}`);
     container.remove();
-  
+    updateCartQuantity();
   });
 });
 
