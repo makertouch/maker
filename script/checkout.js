c72ec0260ed86e411fbdff29cf2a7541f37a8b5b
makertouch <153662956+makertouch@users.noreply.github.com>
2024-03-30 15:05:46 +0300

    Update checkout.js

--- script/checkout.js
+++ script/checkout.js
@@ -128,7 +128,7 @@ document.querySelectorAll(`.js-save-link`).forEach((saveButton) => {
 	const container = document.querySelector(`.js-cart-item-container-${productId}`);
 	container.classList.remove(`is-editing-quantity`);
 		const quantityInput = document.querySelector(`.js-quantity-input-${productId}`);
-		console.log(quantityInput);
+		console.log(quantityInput.value);
 	});
 });
 
