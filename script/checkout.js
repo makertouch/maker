5e933b388331113ef6d56ba9598768e58dbe515b
makertouch <153662956+makertouch@users.noreply.github.com>
2024-04-10 10:36:21 +0300

    priceCents

--- script/checkout.js
+++ script/checkout.js
@@ -208,7 +208,7 @@ function paymantSummary() {
 
   deliveryOptions.forEach((option) => {
     if(option.id === deliveryOptionId) {
-      shippingPrice += formatCurrency(option.priceCents);
+      shippingPrice += option.priceCents;
     }
   });
 
@@ -216,7 +216,7 @@ function paymantSummary() {
 
   products.forEach((product) => {
     if (product.id === productId) {
-      priceSummary += formatCurrency(product.priceCents);
+      priceSummary += product.priceCents;
     }
   });
 
