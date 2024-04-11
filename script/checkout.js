62826fbbdfdb77e07d54aa22fb67b17d0f6ee0df
makertouch <153662956+makertouch@users.noreply.github.com>
2024-04-11 18:44:33 +0300

    render

--- script/checkout.js
+++ script/checkout.js
@@ -128,8 +128,8 @@ document.querySelectorAll(`.js-delete-link`).forEach((link) => {
     const container = document.querySelector(`.js-cart-item-container-${productId}`);
     container.remove();
     updateCart();
-    renderOrderSummary();
   });
+  renderOrderSummary();
 });
 
 
