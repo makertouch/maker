521dd0280471b77cb44802d57827496786bdff43
makertouch <153662956+makertouch@users.noreply.github.com>
2024-04-21 17:41:14 +0300

    d

--- script/checkout.js
+++ script/checkout.js
@@ -1,4 +1,4 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
-import {renderPaymentSummary} from './checkout/OrderSummary.js';
+import {renderPaymentSummary} from './checkout/paymantSummary.js';
 renderOrderSummary();
 renderPaymentSummary();
