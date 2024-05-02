578ef35a8ce0bac6252f564fdc11f6fdb2d6f77d
makertouch <153662956+makertouch@users.noreply.github.com>
2024-05-02 07:33:48 +0300

    s

--- script/checkout/orderSummary.js
+++ script/checkout/orderSummary.js
@@ -2,7 +2,7 @@ import { cart, removeFromCart, updateQuantity, updateDeliveryOption } from '../.
 import { products, getProduct } from '../../data/products.js';
 import {formatCurrency} from '../utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
-import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
+import {deliveryOptions, getDeliveryOption, getDeliveryOptionDate} from '../../data/deliveryOptions.js';
 import { renderPaymentSummary } from './paymentSummary.js';
 
 export function renderOrderSummary() {
