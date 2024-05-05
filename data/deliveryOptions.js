e22cd6863b416c36684359b070bc125d38234f31
makertouch <153662956+makertouch@users.noreply.github.com>
2024-05-05 07:38:27 +0300

    1

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -32,9 +32,7 @@ export function getDeliveryOptionDate(deliveryOption) {
   while (remainingDays > 0) {  // All the scope exists as long as remainingDays > 0
     deliveryDate = deliveryDate.add(1, `day`);
 
-    if (isWeekend(deliveryDate)) {
-      remainingDays++;
-    } else {
+    if (!isWeekend(deliveryDate)) {
       remainingDays--;
     }
   }
