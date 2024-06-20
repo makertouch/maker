1759306c76ad4b693b367219217b17dd963abf21
makertouch <153662956+makertouch@users.noreply.github.com>
2024-06-20 08:58:38 +0300

    fgd

--- first projects/HTML CSS course/Dispenser/script.js
+++ first projects/HTML CSS course/Dispenser/script.js
@@ -11,7 +11,6 @@ codeArray = code.split(` `);
 
  renderNumbers(codeArray);   
 
-console.log(codeArray);
 });
 
 
@@ -20,18 +19,20 @@ let html = ``;
 
 function renderNumbers(codeArray) {
 
-for (i = 0; i < codeArray.leangth; i += 2) {
+for (i = 0; i < codeArray.length; i += 2) {
 
 if (i + 1 < codeArray.length) {
 
-let part1 = Number([i]);
-let part2 = Number([i + 1]);
+let part1 = Number(codeArray[i]);
+let part2 = Number(codeArray[i + 1]);
 
-html += `Dot ${part1 + 1} ${part2 + 3} 0.200 0.000`
-
-
+html += `Dot ${part1 + 1} ${part2 + 3} 0.200 0.000`;
 }
 
 }
+console.log(codeArray);
 console.log(html);
 }
+
+
+
