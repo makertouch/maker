54e8f8539c20fc1c6df5b24338077a1ec6a288ba
makertouch <153662956+makertouch@users.noreply.github.com>
2024-06-20 12:38:51 +0300

    ghj

--- first projects/HTML CSS course/Dispenser/script.js
+++ first projects/HTML CSS course/Dispenser/script.js
@@ -21,18 +21,21 @@ function renderNumbers(codeArray) {
 
 for (i = 0; i < codeArray.length; i += 2) {
 
-if (i + 1 < codeArray.length) {
+if (i + 1 < codeArray.length) { 
 
-let part1 = Number(codeArray[i]);
-let part2 = Number(codeArray[i + 1]);
+let part1 = Number(codeArray[i]).toFixed(2);
+let part2 = Number(codeArray[i + 1]).toFixed(2);
 
-html += `Dot ${part1 + 1} ${part2 + 3} 0.200 0.000`;
+html += 
+`<div>
+Dot ${part1 + 1} ${part2 + 3} 0.200 0.000
+</div>`
 }
-
 }
+document.querySelector(`.result`).innerHTML = html;
 console.log(codeArray);
-console.log(html);
 }
+
 
 
 
