27307aea10e240104c06daa93e7ed1386963ff24
makertouch <153662956+makertouch@users.noreply.github.com>
2024-06-29 21:12:51 +0300

    fds

--- first projects/Todo List Work/script/todoList.js
+++ first projects/Todo List Work/script/todoList.js
@@ -1,178 +1,85 @@
+const tasks = {
+    prints: [],
+    glueBlock: [],
+    ploter: [],
+    engineers: [],
+    urgent: [],
+    others: []
+};
 
-const prints = [];
-const glueBlock = [];
-const ploter = [];
-const engineers = [];
-const urgent = [];
-const others = [];
+const taskInput = document.querySelector(`.text-input`);
 
-const task = document.querySelector(`.text-input`);
+const buttons = {
+    addButton: document.querySelector(`.add-button`),
+    printsButton: document.querySelector(`.print-sort`),
+    glueBlockButton: document.querySelector(`.glue-block-sort`),
+    ploterButton: document.querySelector(`.ploter-sort`),
+    engineersButton: document.querySelector(`.engineers-sort`),
+    urgentButton: document.querySelector(`.urgent-units-sort`)
+};
 
+const tasksList = [];
 
-const addButton = document.querySelector(`.add-button`);
-const printsButton = document.querySelector(`.print-sort`);
-const glueBlockButton = document.querySelector(`.glue-block-sort`);
-const ploterButton = document.querySelector(`.ploter-sort`);
-const engineersButton = document.querySelector(`.engineers-sort`);
-const urgentButton = document.querySelector(`.urgent-units-sort`);
-
-
-addButton.addEventListener(`click`, () => {
-    others.push(task.value);
-    renderHTML();
-});
-
-printsButton.addEventListener(`click`, () => {
-    if (task.value) {
-        prints.push(task.value);
+function addTaskToCategory(category) {
+    if (taskInput.value) {
+        const newTask = {
+            task: taskInput.value,
+            category: category,
+            timestamp: new Date().getTime()
+        };
+        tasks[category].unshift(newTask.task);  // Add to the specific category array
+        tasksList.unshift(newTask);  // Add to the overall list with timestamp
         renderHTML();
     }
-});
-
-glueBlockButton.addEventListener(`click`, () => {
-    if (task.value) {
-        glueBlock.push(task.value);
-        renderHTML();
-    }
-});
-
-ploterButton.addEventListener(`click`, () => {
-    if (task.value) {
-        ploter.push(task.value);
-        renderHTML();
-    }
-});
-
-engineersButton.addEventListener(`click`, () => {
-    if (task.value) {
-        engineers.push(task.value);
-        renderHTML();
-    }
-});
-
-urgentButton.addEventListener(`click`, () => {
-    if (task.value) {
-        urgent.push(task.value);
-        renderHTML();
-    }
-});
-
-let html = ``;
-
-function renderHTML() {
-    let html = ``;
-
-    prints.forEach((task) => {
-        html += `
-<div class="todo">
-    <div class="left-part">
-        <input type="checkbox">
-        <div class="todo-3d-prints">
-            ${task}
-        </div>
-    </div>
-    <div class="right-part">
-        <button class="button-edit">Edit</button>
-        <button class="button-priority">Priority</button>
-    </div>
-</div>
-        `;
-    });
-
-    glueBlock.forEach((task) => {
-        html += `
-<div class="todo">
-    <div class="left-part">
-        <input type="checkbox">
-        <div class="todo-glue-block">
-            ${task}
-        </div>
-    </div>
-    <div class="right-part">
-        <button class="button-edit">Edit</button>
-        <button class="button-priority">Priority</button>
-    </div>
-</div>
-        `;
-    });
-
-    ploter.forEach((task) => {
-        html += `
-<div class="todo">
-    <div class="left-part">
-        <input type="checkbox">
-        <div class="todo-ploter">
-            ${task}
-        </div>
-    </div>
-    <div class="right-part">
-        <button class="button-edit">Edit</button>
-        <button class="button-priority">Priority</button>
-    </div>
-</div>
-        `;
-    });
-
-    engineers.forEach((task) => {
-        html += `
-<div class="todo">
-    <div class="left-part">
-        <input type="checkbox">
-        <div class="todo-engineers">
-            ${task}
-        </div>
-    </div>
-    <div class="right-part">
-        <button class="button-edit">Edit</button>
-        <button class="button-priority">Priority</button>
-    </div>
-</div>
-        `;
-    });
-
-    urgent.forEach((task) => {
-        html += `
-<div class="todo">
-    <div class="left-part">
-        <input type="checkbox">
-        <div class="todo-urgent">
-            ${task}
-        </div>
-    </div>
-    <div class="right-part">
-        <button class="button-edit">Edit</button>
-        <button class="button-priority">Priority</button>
-    </div>
-</div>
-        `;
-    });
-
-    others.forEach((task) => {
-        html += `
-<div class="todo">
-    <div class="left-part">
-        <input type="checkbox">
-        <div class="todo-others">
-            ${task}
-        </div>
-    </div>
-    <div class="right-part">
-        <button class="button-edit">Edit</button>
-        <button class="button-priority">Priority</button>
-    </div>
-</div>
-        `;
-    });
-
-document.querySelector(`.todo-list-container`).innerHTML = html;
-task.value = ``;
 }
 
- 
+buttons.addButton.addEventListener('click', () => {
+    addTaskToCategory('others');
+});
 
+buttons.printsButton.addEventListener('click', () => {
+    addTaskToCategory('prints');
+});
 
+buttons.glueBlockButton.addEventListener('click', () => {
+    addTaskToCategory('glueBlock');
+});
 
+buttons.ploterButton.addEventListener('click', () => {
+    addTaskToCategory('ploter');
+});
 
+buttons.engineersButton.addEventListener('click', () => {
+    addTaskToCategory('engineers');
+});
 
+buttons.urgentButton.addEventListener('click', () => {
+    addTaskToCategory('urgent');
+});
 
-//.glueBlock.ploter.engineers.urgent.others
+function renderHTML() {
+    let html = '';
+
+    // Sort tasksList by timestamp in descending order
+    const sortedTasks = tasksList.sort((a, b) => b.timestamp - a.timestamp);
+
+    sortedTasks.forEach(taskObj => {
+        html += `
+<div class="todo">
+    <div class="left-part">
+        <input type="checkbox">
+        <div class="todo-${taskObj.category}">
+            ${taskObj.task}
+        </div>
+    </div>
+    <div class="right-part">
+        <button class="button-edit">Edit</button>
+        <button class="button-priority">Priority</button>
+    </div>
+</div>
+        `;
+    });
+
+    document.querySelector(`.todo-list-container`).innerHTML = html;
+    taskInput.value = '';
+}
