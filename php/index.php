b0cb43d7e62403e1a4dc9e77386ef16356144ebe
makertouch <153662956+makertouch@users.noreply.github.com>
2024-08-23 11:54:14 +0300

    gvd

--- php/index.php
+++ php/index.php
@@ -4,23 +4,19 @@ require 'functions.php';
 
 // require 'router.php';
 
-
 // connect to MySQL database.
 
 $dsn = "mysql:host=p3nlmysql139plsk.secureserver.net;port=3306;dbname=makertouch_tests;charset=utf8mb4;";
 
+$pdo = new PDO($dsn, 'makertouch_tests', 'TopMaker');
 
-$pdo = new PDO($dsn, 'makertouch', 'TopMaker10');
-
-
-$statement = $pdo -> prepare("SELECT * FROM posts");
+$statement = $pdo->prepare("SELECT * FROM posts");
 $statement->execute();
-
 
 $posts = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 foreach ($posts as $post) {
-    echo "<li>" . $post['title'] . "</li>"; 
+    echo "<li>" . $post['titles'] . "</li>"; 
 }
 
 ?>
