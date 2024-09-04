be707db2eb05f208998ec56ea7d53b9bc2e46260
makertouch <153662956+makertouch@users.noreply.github.com>
2024-09-04 11:37:58 +0300

    gf

--- php/index.php
+++ php/index.php
@@ -9,13 +9,14 @@ require 'functions.php';
 
 class Database {
 
-    public $connection;
+    public $connection; // Created by me or automaticlly by PHP.
 
     public function __construct() {
 
         $dsn = "mysql:host=p3nlmysql139plsk.secureserver.net;port=3306;dbname=makertouch_tests;charset=utf8mb4;";
 
         $this->connection = new PDO($dsn, 'makertouch_tests', 'TopMaker');
+       
     }
 
     public function query($query) {
@@ -26,10 +27,11 @@ class Database {
         return $statement->fetchAll(PDO::FETCH_ASSOC);
     }
 }
- עכ
+ 
 
-$db = new Database();
-$posts = $db->query("select * from posts");
+$db = new Database(); // connects to the database.
+
+$posts = $db->query("select * from posts"); // calls a mothod which gives all the posts.
 
 
 foreach ($posts as $post) {
