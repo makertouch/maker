aa9ab1c3d9273f977aed0e5fe3b63cee413f9a02
makertouch <153662956+makertouch@users.noreply.github.com>
2024-09-13 11:05:28 +0300

    ;lk

--- php/controllers/note.php
+++ php/controllers/note.php
@@ -8,8 +8,6 @@ $id = $_GET['id'];
 $config = require 'config.php';
 
 $db = new Database($config['database']); // connects to the database.
-
-
 $note = $db->query('select * from notes where id= ?', [$id])->findOrFail();  // $note = [ "id" => "1", "body" => "message", "user_id" => "1"]
 
 // if there was no return $this in the query method:
