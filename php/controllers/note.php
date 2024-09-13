88512bea60aeb7dc172343ff5db909ba54daedac
makertouch <153662956+makertouch@users.noreply.github.com>
2024-09-13 09:43:48 +0300

    lj

--- php/controllers/note.php
+++ php/controllers/note.php
@@ -12,6 +12,9 @@ $id = $_GET['id'];
 
 $note = $db->query('select * from notes where id= ?', [$id])->findOrFail();  // $note = [ "id" => "1", "body" => "message", "user_id" => "1"]
 
+// if there was no return $this in query method:
+// $db->query('SELECT * FROM notes WHERE id = ?', [$id]); 
+// $note = $db->findOrFail();
 
 
 if($note['user_id'] != $currentUser) { // using != sign for 1 can be in any type, same with ==. 
