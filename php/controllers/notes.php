54e76a81782f2224f31122e380f598df378e9008
makertouch <153662956+makertouch@users.noreply.github.com>
2024-09-19 13:58:16 +0300

    hhhg

--- php/controllers/notes.php
+++ php/controllers/notes.php
@@ -9,7 +9,7 @@ $db = new Database($config['database']); // connects to the database.
 
 $notes = $db->query('select * from notes where user_id = 1')->get();
 
-require 'views/notes/index.view.php'; // the view link will always be in the end.
+require 'views/notes/index.view.php'; // the view link will always be in the end. 
 
 ?>
 
