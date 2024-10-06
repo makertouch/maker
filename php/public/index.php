3b628f629d2471f7e7ad421da5059873a2f1b6b4
makertouch <153662956+makertouch@users.noreply.github.com>
2024-10-06 15:28:05 +0300

    dsd

--- php/public/index.php
+++ php/public/index.php
@@ -6,7 +6,7 @@ const BASE_PATH = __DIR__ . '/../';
 require BASE_PATH . 'functions.php'; 
 
 spl_autoload_register(function ($class) { 
- require base_path($class);
+ require base_path($class . '.php');
 
  });
 
