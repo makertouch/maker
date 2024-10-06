dfdcb789758aa7aca1baf026d398752014ba1c59
makertouch <153662956+makertouch@users.noreply.github.com>
2024-10-06 15:37:33 +0300

    Update index.php

--- php/public/index.php
+++ php/public/index.php
@@ -5,15 +5,13 @@ const BASE_PATH = __DIR__ . '/../';
 
 require BASE_PATH . 'functions.php'; 
 
-function myAutoloader($class) {
-    require base_path($class . '.php');
-}
+spl_autoload_register(function ($class) { 
+ require base_path($class . '.php');
 
-spl_autoload_register('myAutoloader');
-
+ });
 
 require base_path('router.php');
 
-//test
+
 
 ?>
