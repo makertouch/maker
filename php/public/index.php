71c56fb4b0529da6ac07a6183e174ccbdbd5d253
makertouch <153662956+makertouch@users.noreply.github.com>
2024-10-07 19:24:38 +0300

    Update index.php

--- php/public/index.php
+++ php/public/index.php
@@ -6,6 +6,10 @@ const BASE_PATH = __DIR__ . '/../';
 require BASE_PATH . 'core/functions.php'; 
 
 spl_autoload_register(function ($class) { 
+ // $class = core\Database
+// str_replace($search, $replace, $subject);
+   $class = str_replace('\\', '/', $class);
+ 
  require base_path('core/' . $class . '.php');
  });
 
