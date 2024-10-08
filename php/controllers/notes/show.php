ef79076adfe3cdba4ec575b994ed4bd7e268797f
makertouch <153662956+makertouch@users.noreply.github.com>
2024-10-08 18:27:11 +0300

    Update show.php

--- php/controllers/notes/show.php
+++ php/controllers/notes/show.php
@@ -6,7 +6,7 @@ $id = $_GET['id'];
 
 $config = require base_path('config.php');
 
-$db = new Database($config['database']); // connects to the database.
+$db = new core\Database($config['database']); // connects to the database.
 $note = $db->query('select * from notes where id= ?', [$id])->findOrFail();  // $note = [ "id" => "1", "body" => "message", "user_id" => "1"]
 
 authorize($note['user_id'] != $currentUser);
