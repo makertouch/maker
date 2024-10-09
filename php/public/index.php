d0da977ff95701d66a6b76f623af44dff12ac165
makertouch <153662956+makertouch@users.noreply.github.com>
2024-10-09 19:41:06 +0300

    Update index.php

--- php/public/index.php
+++ php/public/index.php
@@ -6,6 +6,7 @@ const BASE_PATH = __DIR__ . '/../';
 require BASE_PATH . 'core/functions.php'; 
 
 spl_autoload_register(function ($class) {   // $class = core\Database
+                                         // spl just recognize inatances and pass it on
                                                                               
  $result = str_replace('\\', '/', $class); // str_replace($search, $replace, $subject);
  
