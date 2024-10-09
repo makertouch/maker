c046738c06033e3cd39e44400248f2af3e6778e0
makertouch <153662956+makertouch@users.noreply.github.com>
2024-10-09 19:34:13 +0300

    Update index.php

--- php/public/index.php
+++ php/public/index.php
@@ -6,7 +6,9 @@ const BASE_PATH = __DIR__ . '/../';
 require BASE_PATH . 'core/functions.php'; 
 
 spl_autoload_register(function ($class) {   // $class = core\Database
-// The all point of spl_autoload_register is to indentify the $class(OOP);
+                                         
+// The point of spl_autoload_register is to automatically load the class file when a class is instantiated.
+                                         
  $result = str_replace('\\', '/', $class); // str_replace($search, $replace, $subject);
  
  require base_path($result . '.php');
