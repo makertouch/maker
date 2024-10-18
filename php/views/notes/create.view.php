ff701c09ca5558a5b4933c0ac4e31134bd6af516
makertouch <153662956+makertouch@users.noreply.github.com>
2024-10-18 15:46:08 +0300

    Update create.view.php

--- php/views/notes/create.view.php
+++ php/views/notes/create.view.php
@@ -5,7 +5,6 @@
 <?php require base_path('views/partial/header.php') ?>
 
   <main>
- 
 
 <form method="POST">
 	<input type="hidden" name="_method" value="DELETE">
@@ -14,7 +13,7 @@
           <label for="body" class="block text-sm font-medium leading-6 text-gray-900">Body</label>
           <div class="mt-2" style="width: 400px;">
             <textarea 
-					  id="body"
+				  id="body"
 					  name="body" 
 					  rows="3"
 					  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"> <?= isset($_POST['body']) ? $_POST['body'] : '' ?> </textarea>
@@ -30,15 +29,8 @@
     <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
     <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
   </div>
-	</div>
-	
+	</div>	
 </form>
-
-
-
-
-
-	  
 
   </main>
 
